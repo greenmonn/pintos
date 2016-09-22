bcf6b84bae7978164f3e8075e1753f644074f277
greenmon <greenmon@kaist.ac.kr>
2016-09-22 12:01:29 +0400

    Priority error solved - missed case thread_yield()

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -314,7 +314,7 @@ thread_yield (void)
 
   old_level = intr_disable ();
   if (curr != idle_thread) 
-    list_push_back (&ready_list, &curr->elem);
+    list_insert_ordered (&ready_list, &curr->elem, list_more_priority, NULL);
   curr->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
